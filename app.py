7f1ca81243031108798268897d5262477ad7af84
cstlford <cstlford@gmail.com>
2024-09-23 17:11:12 -0400

    fixed nutrition functions and integration into app

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, jsonify
 from gemini_api import generate
+import nutrition_functions as nf
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     ## Testing LLM Function ##
-    data = {
-        'target_calories': 2500, 'macros': {'protein': 128, 'fat': 78, 'carbs': 323}
+    dummy_data = {
+    "weight": nf.lbs_to_kg(150),
+    "height": nf.inches_to_cm(66),
+    "age": 24,
+    "is_male": True,
+    "goal": "gain",
+    "diet_type": "carnivore",
+    "activity_level": "sedentary"
     }
+    data = nf.get_nutrition_plan(dummy_data)
     response = generate(data)
     ## Testing LLM Function ##
     
